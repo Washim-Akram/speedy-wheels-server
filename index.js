6cb68b98391d2bcc1091b64a925af2a9edbddd6b
Washim-Akram <muhammadwashimakram@gmail.com>
2023-05-20 18:43:07 +0600

    Made an get api and serve data to  get request by specific id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -30,6 +30,13 @@ async function run() {
 
     app.get("/cars", async(req, res) => {
         const result = await carCollection.find().limit(20).toArray();
+        res.send(result);
+    });
+
+    app.get("/car/:id", async(req, res) => {
+        const id = req.params.id;
+        const query = { _id : new ObjectId(id)}
+        const result = await carCollection.findOne(query);
         res.send(result);
     });
 
